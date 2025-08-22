2d20074a254a7baa3715e02b22040c9437faf65c
João Vidal <dev.joaovidal@gmail.com>
2025-08-21 22:16:43 -0300

    adjusts

--- backend/src/main/java/com/joaovidal/blog/controllers/AuthController.java
+++ backend/src/main/java/com/joaovidal/blog/controllers/AuthController.java
@@ -6,12 +6,16 @@ import com.joaovidal.blog.models.dtos.LoginResponse;
 import com.joaovidal.blog.models.dtos.LoginRequest;
 import com.joaovidal.blog.models.dtos.RegisterRequest;
 import com.joaovidal.blog.services.AuthenticationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/auth")
@@ -25,9 +29,9 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<User> register(@RequestBody RegisterRequest request) {
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
         User created = authService.signup(request);
-        return ResponseEntity.ok(created);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
